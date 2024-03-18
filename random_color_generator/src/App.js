63afaffb12ff4b90e835522eb5c73c42a6a36af2
My name <me@example.com>
2024-03-18 07:58:16 -0700

    star rating

---
+++ random_color_generator/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RandomColor from './components/random-color'
+
+const App = () => {
+  return (
+    <div>
+        {RandomColor}
+    </div>
+  )
+}
+
+export default App
